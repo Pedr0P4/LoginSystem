f1f85c72730807837b19444d737799e49cd2959b
Pedro Paulo <pedropaulo062011@gmail.com>
2025-09-25 00:14:01 -0300

    :heavy_plus_sign: build: Installing dependencies

--- login-system/src/App.tsx
+++ login-system/src/App.tsx
@@ -1,10 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-
 function App() {
   return (
-
+    <div>
+      <h1>Hello, World!</h1>
+    </div>
   );
 }
 
