0928fb88c0ded7c7ade6b9c9c9a0506ded6942d6
Pedro Paulo <pedropaulo062011@gmail.com>
2025-09-26 01:13:42 -0300

    :sparkles: feat(login): Login form created

--- login-system/src/App.tsx
+++ login-system/src/App.tsx
@@ -1,7 +1,11 @@
+import Login from "@/components/project/login";
+
 function App() {
   return (
-    <div>
-      <h1>Hello, World!</h1>
+    <div className="w-screen h-screen bg-blue-50 flex items-center">
+      <div className="mx-auto w-1/4 h-1/2 min-w-75 min-h-92 bg-white rounded-xl p-8 shadow-2xl">
+        <Login />
+      </div>
     </div>
   );
 }
